fe47ad85ff7fb4bdf6f9bfd70a7140806c92015c
Tarek <Tarek@PC-Tarek>
2013-03-11 14:03:35 +0100

    11/03

--- edu.esprit.pfeProjectWeb/src/ctrl/CreateArticleCtrl.java
+++ edu.esprit.pfeProjectWeb/src/ctrl/CreateArticleCtrl.java
@@ -1,54 +1,36 @@
 package ctrl;
 
-import java.util.Properties;
-
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+import javax.faces.context.FacesContext;
 
 import edu.esprit.pfeProjectEJB.DAO.interfaces.ArticleDaoRemote;
 import edu.esprit.pfeProjectEJB.persistence.Article;
 
 @ManagedBean
 @SessionScoped
-
 public class CreateArticleCtrl {
 	private Article article = new Article();
-	ArticleDaoRemote remote ;
-	
-	public String doCreateArticle()
-	{
-		getProxy() ;
-		remote.createArticle(article) ;
-		return "" ;
-		
+	@EJB
+	ArticleDaoRemote remote;
+
+	public String doAddArticle() {
+		FacesContext context=FacesContext.getCurrentInstance();
+try { 
+		remote.createArticle(article);
+		article= new Article();
+		FacesMessage message=new FacesMessage(FacesMessage.SEVERITY_INFO,"Info","Création réuissite");
+		context.addMessage(null, message);
+}catch(Exception e){
+
+			FacesMessage message=new FacesMessage(FacesMessage.SEVERITY_ERROR,"Erreur","Erreur de création");
+			context.addMessage(null, message);
+}
+		return "";
 	}
-	
-	public void getProxy()
-	{
-		Properties properties=new Properties();
-		properties.put("java.naming.factory.url.pkgs", "org.jboss.ejb.client.naming") ;
-		properties.put("java.naming.factory.initial", "org.jboss.naming.remote.client.InitialContextFactory");
-		properties.put("java.naming.provider.url", "remote://localhost:4447");
-		properties.put("jboss.naming.client.ejb.context", true);
-		properties.put("jboss.naming.client.connect.options.org.xnio.Options.SASL_POLICY_NOPLAINTEXT", false);
-		
-		try {
-			Context context =new InitialContext(properties) ;
-			remote=(ArticleDaoRemote) context.lookup("/edu.esprit.pfeProject/edu.esprit.pfeProjectEJB/ArticleDao!edu.esprit.pfeProjectEJB.DAO.interfaces.ArticleDaoRemote");
-			
-		} catch (NamingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	}
-	
-	
-	
+
 	public Article getArticle() {
 		return article;
 	}
@@ -56,7 +38,5 @@ public class CreateArticleCtrl {
 	public void setArticle(Article article) {
 		this.article = article;
 	}
-	
-	
 
 }
